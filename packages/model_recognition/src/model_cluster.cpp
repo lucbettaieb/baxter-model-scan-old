347e8292f16b8fc3e23798515cb4b482446474c8
lucbettaieb <luc.bettaieb@gmail.com>
2015-12-09 19:22:37 -0500

    cosmetic changes

--- packages/model_recognition/src/model_cluster.cpp
+++ packages/model_recognition/src/model_cluster.cpp
@@ -46,33 +46,23 @@ void ModelCluster::addToCluster(ModelPattern &pattern)
 {
   float pat_size = static_cast<float>(patterns.size());
 
-  r_centroid = r_centroid * (pat_size/(pat_size+1)) + ((1/(pat_size+1)) * pattern.getRed());
-  std::cout << "new r: " << r_centroid << std::endl;
+  r_centroid *= pat_size/(pat_size+1);
+  r_centroid += ((1/(pat_size+1)) * pattern.getRed());
 
-  std::cout << "old g: " << g_centroid << std::endl;
   g_centroid *= pat_size/(pat_size+1);
   g_centroid += (1/(pat_size+1)) * pattern.getGreen();
-  std::cout << "new g: " << g_centroid << std::endl;
 
-  std::cout << "old b: " << b_centroid << std::endl;
   b_centroid *= pat_size/(pat_size+1);
   b_centroid += (1/(pat_size+1)) * pattern.getBlue();
-  std::cout << "new b: " << b_centroid << std::endl;
 
-  std::cout << "old l: " << l_centroid << std::endl;
   l_centroid *= pat_size/(pat_size+1);
   l_centroid += (1/(pat_size+1)) * pattern.getLength();
-  std::cout << "new l: " << l_centroid << std::endl;
 
-  std::cout << "old w: " << w_centroid << std::endl;
   w_centroid *= pat_size/(pat_size+1);
   w_centroid += (1/(pat_size+1)) * pattern.getWidth();
-  std::cout << "new w: " << w_centroid << std::endl;
 
-  std::cout << "old h: " << h_centroid << std::endl;
   h_centroid *= pat_size/(pat_size+1);
   h_centroid += (1/(pat_size+1)) * pattern.getHeight();
-  std::cout << "new h: " << h_centroid << std::endl;
 
   std::cout << std::endl;
   patterns.push_back(pattern);
@@ -88,27 +78,22 @@ void ModelCluster::removeFromCluster(ModelPattern pattern)
     if (label == patterns.at(i).getLabel())
       patterns.erase(patterns.begin()+i);
   }
-  
+
   r_centroid *= pat_size/(pat_size-1);
   r_centroid -= (1/(pat_size-1)) * pattern.getRed();
 
-  std::cout << "2" << std::endl;
   g_centroid *= pat_size/(pat_size-1);
   g_centroid -= (1/(pat_size-1)) * pattern.getGreen();
 
-  std::cout << "3" << std::endl;
   b_centroid *= pat_size/(pat_size-1);
   b_centroid -= (1/(pat_size-1)) * pattern.getBlue();
 
-  std::cout << "4" << std::endl;
   l_centroid *= pat_size/(pat_size-1);
   l_centroid -= (1/(pat_size-1)) * pattern.getLength();
 
-  std::cout << "5" << std::endl;
   w_centroid *= pat_size/(pat_size-1);
   w_centroid -= (1/(pat_size-1)) * pattern.getWidth();
 
-  std::cout << "6" << std::endl;
   h_centroid *= pat_size/(pat_size-1);
   h_centroid -= (1/(pat_size-1)) * pattern.getHeight();
 }
